25a154c93e37c5e1091c6cb3b5a572da55ecca6b
Pablo Curumaco <jpablocuru@gmail.com>
2018-10-17 15:48:00 -0600

    Bootstrap 4 & Vuejs
    - Installation with Vuejs

---
+++ resources/js/components/TitleExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        {{title}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: "Bootstrap & Vuejs",
+                subtitle: "Component with Vuejs"
+            }
+        },
+        mounted() {
+            console.log('Title Component mounted.');
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -77,20 +78,26 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <div id="app">
+                <div class="content">
+                    <div class="title m-b-md">
+                        Laravel
+                    </div>
+                    
+                    <title-component></title-component>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="links">
+                        <a href="https://laravel.com/docs">Documentation</a>
+                        <a href="https://laracasts.com">Laracasts</a>
+                        <a href="https://laravel-news.com">News</a>
+                        <a href="https://nova.laravel.com">Nova</a>
+                        <a href="https://forge.laravel.com">Forge</a>
+                        <a href="https://github.com/laravel/laravel">GitHub</a>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="/js/app.js"></script>
     </body>
 </html>
